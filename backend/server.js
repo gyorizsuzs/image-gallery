7b04635148284877c02451cfcacf27d1c04400f4
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-10-04 00:26:59 +0200

    making small adjustments, setting up swiper

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,35 @@ app.get("/images/:id", (req, res) => {
   res.sendFile(path.join(__dirname, getPathById(id)));
 });
 
+app.post("/uploadimage", (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("no file was uploaded");
+  }
+
+  const file = req.files.imageFile;
+  const fileName = req.body.imageName;
+
+  file.mv(`${__dirname}/./images/${fileName}.jpg`, (error) => {
+    if (error) {
+      return res.status(500).send(error);
+    }
+
+    try {
+      let images = fs.readFileSync(`${__dirname}/./images.json`);
+      imageData = JSON.parse(images);
+      imageData.push({ image: `/backend/images/${fileName}.jpg` });
+      fs.writeFileSync(
+        `${__dirname}/./images/images.json`,
+        JSON.stringify(imageData)
+      );
+      res.status(200).send("nice");
+    } catch (error) {
+      console.log(error);
+      res.status(418).send(error);
+    }
+  });
+});
+
 const getPathById = (id) => {
   const images = JSON.parse(fs.readFileSync("./images/images.json"));
   const image = images.find((image) => image.id == id);
--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,42 @@
+import Swiper, { Navigation, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const swiper = new Swiper(".swiper", {
+  direction: "vertical",
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+const swiperComponent = function () {
+  <div class="swiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">Image 1</div>
+      <div class="swiper-slide">Image 2</div>
+      <div class="swiper-slide">Image 3</div>
+    </div>
+    <div class="swiper-pagination"></div>
+
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+
+    <div class="swiper-scrollbar"></div>
+  </div>;
+};
+
 const imageComponent = ({ id, title, uploadDate, photographersName }) => {
   const url = `http://127.0.0.1:3000/images/${id}`;
   return `
@@ -14,6 +53,24 @@ const getImages = async () => {
   const response = await fetch("/images");
   const images = await response.json();
   return images;
+};
+
+const formComponent = function ({ id, title, uploadDate, photographersName }) {
+  return `
+  <div id='main'>
+    <form class="image-upload">
+      <div class="input-container">
+        <input class="id" type="text" placeholder="ID">
+        <input class="title" type="text" placeholder="image name">
+        <input class="uploadDate" type="text" placeholder="date of upload">
+        <input class="photographer" type="text" placeholder="name of photographer">
+        <input class="image-uploader" type="file" placeholder="Upload image" required>
+        <div class="button-container">
+          <button id="image-upload-button">Send</button>
+        </div>
+      </div>
+    </form>
+    </div>`;
 };
 
 console.log(await getImages());
